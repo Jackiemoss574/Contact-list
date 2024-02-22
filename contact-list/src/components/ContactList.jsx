1a02861fb7b287ac0940f577beb921e517c95493
Jackie <Jacqueline.moss23@gmail.com>
2024-02-21 19:38:57 -0600

    update

---
+++ contact-list/src/components/ContactList.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import ContactRow from "./ContactRow";
+
+// Dummy data array defined outside the component
+const dummyContacts = [
+  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+
+];
+
+export default function ContactList() {
+  // Initializing the contacts state with the dummyContacts array
+  const [contacts, setContacts] = useState(dummyContacts);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th colSpan="3">Contact List</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Phone</td>
+        </tr>
+        {contacts.map((contact, index) => (
+          <tr key={index}>
+            <td>{contact.name}</td>
+            <td>{contact.email}</td>
+            <td>{contact.phone}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
